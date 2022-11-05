76be6920d6fc12809155339f6706232af5982a70
joao-lucas-paes <joaozinhopaes12@gmail.com>
2022-11-05 15:03:13 -0400

    conversao de int para char

--- index.cpp
+++ index.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+/// @brief conversao via char do c
+/// @param a ponteiro para a primeira casa do char
+/// @return retorna valor do char
+int charToInt(char* a) {
+    int num = 0;
+
+    for(char* i = a; *i != '\0'; i++)
+        num = (num * 10) + (*(i) - '0');
+
+    return num;
+}
+
 int main(int argc, char *argv[]) {
-    
+    int num = charToInt(argv[1]);
+
 }
