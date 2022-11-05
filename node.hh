a103721b1f74c5c6c7a23bdd7fd3b0c3118ea2c8
joao-lucas-paes <joaozinhopaes12@gmail.com>
2022-11-05 15:58:34 -0400

    node done

--- node.hh
+++ node.hh
@@ -8,8 +8,36 @@ struct node {
     int* count;
 };
 
-void createNode(node* n, int pageNum) {
+void createNode(node* proxN, node* n, string key, int pageNum, int thisPage) {
     n->prox = new node;
     n->prox->count = new int[pageNum];
-    n->prox->prox = nullptr;
+    
+    for(int i = 0; i < pageNum; i++) {
+        n->prox->count[i] = 0;
+    }
+
+    n->prox->count[thisPage] = 1;
+
+    n->prox->value = key;
+    n->prox->prox = proxN;
+}
+
+void sumWord(node* n, int thisPage) {
+    n->count[thisPage] += 1;
+}
+
+void addWord(node* header, string key, int pageNum, int thisPage) {
+    node* i = header->prox;
+    for(;i->prox != header; i = i->prox) {
+        if(i->value.compare(key) == 0) {
+            sumWord(i, thisPage);
+            return;
+        }
+        if(i->prox->value.compare(key) > 0) {
+           createNode(i->prox, i, key, pageNum, thisPage);
+           return;
+        }
+    }
+
+    createNode(header, i, key, pageNum, thisPage);
 }
